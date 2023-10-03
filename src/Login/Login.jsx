5a53df3196ab6e651dad7745d46d819f008b4d3f
Dibyendu Pramanik <chotuu668@gmil.com>
2023-10-04 03:42:21 +0530

    first commit

---
+++ src/Login/Login.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../pages/Shared/Navbar/Navbar";
+
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h1>This login</h1>
+        </div>
+    );
+};
+
+export default Login;
