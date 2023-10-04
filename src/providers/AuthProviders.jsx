2026f9f3c797a362b058ef06d8e516705fc4bcd9
Dibyendu Pramanik <chotuu668@gmil.com>
2023-10-04 21:35:57 +0530

    update

---
+++ src/providers/AuthProviders.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { createContext } from "react";
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut} from "firebase/auth"
+import app from "../firebase/firebase.config";
+import { useEffect } from "react";
+export const AuthContext =  createContext(null);
+
+const AuthProviders = ({children}) => {
+    const auth = getAuth(app);
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] =useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    const signIn =(email, password) =>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+
+    }
+
+    const  logOut = () =>{
+        setLoading(true);
+        return signOut(auth);
+    }
+    useEffect(() =>{
+   const unSubscribe =  onAuthStateChanged(auth, currentUser =>{
+            console.log('user in the auth state changed' , currentUser);
+            setUser(currentUser);
+            setLoading(false);
+        });
+
+        return () =>{
+            unSubscribe();
+        }
+    },[])
+    
+
+    const authInfo ={
+        user,
+        loading,
+        createUser,
+        signIn,
+        logOut
+
+    }
+    
+    return (
+       <AuthContext.Provider value={authInfo}>
+        {children}
+       </AuthContext.Provider>
+    );
+};
+
+export default AuthProviders;
